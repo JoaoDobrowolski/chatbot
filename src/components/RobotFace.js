8d45706a54744ecf6d20a2adb1b545aa2b7fb953
JoaoDobrowolski <joaodobrowolski@outlook.com>
2023-07-19 07:27:54 -0300

    fixed: robotface hidden when doesnt fit in screen

--- src/components/RobotFace.js
+++ src/components/RobotFace.js
@@ -1,12 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import styles from '../styles/robot.module.css';
 import ChatBotContext from '@/context/ChatBotContext';
 
 export default function RobotFace() {
   const { isFiltered, blink } = useContext(ChatBotContext);
 
+  const [isScreenSmall, setIsScreenSmall] = useState(false);
+
+  const checkScreenSize = () => {
+    setIsScreenSmall(window.innerWidth < 600);
+  };
+
+  useEffect(() => {
+    checkScreenSize();
+    window.addEventListener('resize', checkScreenSize);
+    return () => {
+      window.removeEventListener('resize', checkScreenSize);
+    };
+  }, []);
+
   return (
-    <div>
+    <div className={`${styles.robotface} ${isScreenSmall ? 'hidden' : ''}`}>
       <div className="flex justify-center items-center">
         <a href="https://github.com/JoaoDobrowolski/chatbot" rel="noreferrer" target="_blank" className={styles.robotface}>
           <div className={!isFiltered || blink ? styles.leftblink : styles.lefteye}></div>
